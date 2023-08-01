4dcd6ea67bc961997a23ff39a8e097257b919e4a
ZrkllT <zero.kullt@gmail.com>
2023-07-31 21:14:34 -0400

    31-jul 2

--- src/app/config/db.config.js
+++ src/app/config/db.config.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-const conexion = new Sequelize('db_bootcamp', 'tu usuario', 'no vean >:(', {
+const conexion = new Sequelize('db_bootcamp', 'tu usuario', 'tu contraseña', {
     host: 'localhost',
     port: 5432,
     dialect: 'postgres' // a que tipo de bd nos conectamos
